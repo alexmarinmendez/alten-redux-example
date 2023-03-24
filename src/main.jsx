ac12353be5ac16ad40591be88e558a20e06a151b
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-03-24 09:13:14 -0500

    feat: create store and provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './routes/App'
+import { Provider } from 'react-redux'
+import { compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
+import reducer from './reducers'
+
+const initialState = {
+  favoriteCharacters: [],
+  sectionActive: "Characters"
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(reducer, initialState, composeEnhancers(applyMiddleware(logger)))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
