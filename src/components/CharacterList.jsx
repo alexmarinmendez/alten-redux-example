6fe9c787b7cc53dff86699b7002cd340216d1ab0
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-03-24 12:19:05 -0500

    feat: CharacterList implementation (fetch the api)

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -1,6 +1,44 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import "../assets/styles/components/CharacterList.scss"
+import Character from './Character'
+import Loader from './Loader'
+import Error from './Error'
+
 const CharacterList = () => {
+  const [characters, setCharacters] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+
+  const getCharacters = async() => {
+    try {
+      const response = await axios.get('https://rickandmortyapi.com/api/character')
+      const result = response.data.results
+      setCharacters(result)
+      setLoading(false)
+      setError(false)
+    } catch(error) {
+      setLoading(true)
+      setError(true)
+    }
+  }
+
+  useEffect(() => {
+    getCharacters()
+  }, [])
+
   return (
-    <div>CharacterList</div>
+    <div>
+      {
+        loading 
+        ? <Loader /> 
+        : error 
+          ? <Error /> 
+          : (
+            characters.map(item => <Character />)
+          )
+      }
+    </div>
   )
 }
 
