a07f897be23c0057016303d736a7240b735e7514
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-03-25 05:43:44 -0500

    feat: implements Error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,6 +1,12 @@
+import "../assets/styles/components/Error.scss"
+import errorImage from '../assets/static/images/error.png'
+
 const Error = () => {
   return (
-    <div>Error</div>
+    <div className="error">
+      <img src={errorImage} alt="Morty scared" className="error-img"/>
+      <span className="error-text">Sorry, an error has ocurred.</span>
+    </div>
   )
 }
 
