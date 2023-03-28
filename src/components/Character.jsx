97d3af82785d2d086dd3c707658ef5f768656e52
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-03-28 05:14:34 -0500

    feat: implements Character component

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,6 +1,30 @@
-const Character = () => {
+import { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
+import { setFavorite, deleteFavorite } from '../actions'
+import "../assets/styles/components/Character.scss"
+import CharacterDetail from './CharacterDetail'
+import Modal from './Modal'
+
+const Character = (props) => {
+  const [modal, setModal] = useState(false)
+  const [favorite, setFavorite]  = useState(false)
+
+  const { data } = props
+  const { id, image, name, status, gender, species } = data
+  
+  const handleCloseModal = () => {}
+  const handleOpenModal = () => {}
+  const handleSetFavorite = () => {}
+  const handleDeleteFavorite = () => {}
+
+  useEffect(() => {
+
+  }, [])
+
   return (
-    <div>Character</div>
+    <div className='character'>
+      <img className='character__img' src={image} alt="" />
+    </div>
   )
 }
 
