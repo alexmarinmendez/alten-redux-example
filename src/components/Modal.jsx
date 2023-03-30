74fd7d961a3319e718c59f22cde28e6113506e97
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-03-30 04:49:04 -0500

    feat: implements Portal in Modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,17 @@
-const Modal = () => {
-  return (
-    <div>Modal</div>
+import { createPortal } from "react-dom"
+import "../assets/styles/components/Modal.scss"
+import { ReactComponent as SVGClose } from "../assets/static/icons/close.svg"
+
+const Modal = (props) => {
+  props.isOpen
+  ? createPortal(
+    <div className="modal">
+      <figure onClick={props.onClose} className="modal__close">
+        <SVGClose className="modal__close-icon" />
+      </figure>
+    </div>
   )
+  : null
 }
 
 export default Modal
