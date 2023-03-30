f1a63b608574f09bdb90aa2065bffd004c0cafe0
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-03-30 10:58:51 -0500

    feat: implement mapStateToProps in FavoriteList component

--- src/components/FavoriteList.jsx
+++ src/components/FavoriteList.jsx
@@ -1,7 +1,18 @@
-const FavoriteList = () => {
+import { connect } from 'react-redux'
+import "../assets/styles/components/FavoriteList.scss"
+
+const FavoriteList = (props) => {
+  const { favoriteCharacters } = props
+
   return (
     <div>FavoriteList</div>
   )
 }
 
-export default FavoriteList
+const mapStateToProps = state => {
+  return {
+    favoriteCharacters: state.favoriteCharacters,
+  }
+}
+
+export default connect(mapStateToProps, null)(FavoriteList)
