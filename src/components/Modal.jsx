90fa70535faa39bc8b6dc7369d8e5ba3021297b1
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-03-30 05:29:47 -0500

    fix: return JSX in Modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,17 +3,23 @@ import "../assets/styles/components/Modal.scss"
 import { ReactComponent as SVGClose } from "../assets/static/icons/close.svg"
 
 const Modal = (props) => {
-  props.isOpen
-  ? createPortal(
-    <div className="modal">
-      <figure onClick={props.onClose} className="modal__close">
-        <SVGClose className="modal__close-icon" />
-      </figure>
-      <div>{ props.children }</div>
-    </div>,
-    document.getElementById("modal")
+  return (
+    <>
+    {
+      props.isOpen
+      ? createPortal(
+        <div className="modal">
+          <figure onClick={props.onClose} className="modal__close">
+            <SVGClose className="modal__close-icon" />
+          </figure>
+          <div>{ props.children }</div>
+        </div>,
+        document.getElementById("modal")
+      )
+      : null
+    }
+    </>
   )
-  : null
 }
 
 export default Modal
