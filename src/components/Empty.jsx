e0f6428bfa431b4d169370bce95f97966ae0c394
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-03-30 11:06:45 -0500

    feat: implements Empty component

--- src/components/Empty.jsx
+++ src/components/Empty.jsx
@@ -1,6 +1,16 @@
+import "../assets/styles/components/Empty.scss"
+import emptyImage from "../assets/static/images/empty.png"
+
 const Empty = () => {
   return (
-    <div>Empty</div>
+    <div className="empty">
+      <img src={emptyImage} alt="" className="empty-img" />
+      <span className="empty-text">
+        You haven't favorites.
+        <br />
+        Go and select your favorite characters.
+      </span>
+    </div>
   )
 }
 
